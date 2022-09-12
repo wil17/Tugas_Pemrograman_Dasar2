7396e5c07c52c8689470a49edd303c65369199e0
wil17 <willynurgian17@gmail.com>
2022-09-13 07:20:30 +0800

    first commit

---
+++ tugas harian1_Willy Nurgian_2110131210006.java
@@ -0,0 +1,8 @@
+public class tugas harian1_Willy Nurgian_2110131210006 {
+    public static void main(String[] args){
+    System.out.println("NIM : 2110131210006");
+    System.out.println("NAMA : WILLY NURGIAN");
+    System.out.println("ALAMAT : JL.IR PHM NOOR GG.H.HAMDI NO.46");
+    System.out.println("MATA KULIAH YANG DIKUASAI : PEMROGRAMAN WEB");
+    }
+}
